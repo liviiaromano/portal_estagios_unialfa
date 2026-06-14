a016372b3b273375636582b63b5d1f77f583503b
Isahipoliti <belly.lucantonio@gmail.com>
2026-06-14 20:38:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+header("Location: login-aluno-empresa.php");
+exit;
---
+++ login-aluno-empresa.php
@@ -0,0 +1,170 @@
+<?php
+session_start();
+
+if(isset($_POST['entrar_aluno']))
+{
+    $_SESSION['aluno'] = "Aluno UniALFA";
+
+    header("Location: aluno/home.php");
+    exit;
+}
+
+if(isset($_POST['entrar_empresa']))
+{
+    $_SESSION['empresa'] = "Empresa Parceira";
+
+    header("Location: empresa/home.php");
+    exit;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Login Aluno</title>
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+<div class="login-container">
+
+    <div class="login-card">
+
+        <img src="assets/img/logo-unialfa.png" alt="UniALFA">
+
+        <div class="tipo-login">
+
+    <button
+        type="button"
+        id="btnAluno"
+        class="aba ativa"
+        onclick="mostrarAluno()">
+        Aluno
+    </button>
+
+    <button
+        type="button"
+        id="btnEmpresa"
+        class="aba"
+        onclick="mostrarEmpresa()">
+        Empresa
+    </button>
+
+        </div>
+
+        <div id="formAluno">
+
+    <h2>Portal do Aluno</h2>
+
+    <form method="POST">
+
+        <div class="form-group">
+            <label>Email</label>
+
+            <input
+                type="email"
+                name="email"
+                class="form-control"
+                required>
+        </div>
+
+        <div class="form-group">
+            <label>Senha</label>
+
+            <input
+                type="password"
+                name="senha"
+                class="form-control"
+                required>
+        </div>
+
+        <button
+            type="submit"
+            name="entrar_aluno"
+            class="btn-primary">
+            Entrar
+        </button>
+
+    </form>
+
+</div>
+
+
+<div id="formEmpresa" style="display:none;">
+
+    <h2>Painel da Empresa</h2>
+
+    <form method="POST">
+
+        <div class="form-group">
+            <label>Email Corporativo</label>
+
+            <input
+                type="email"
+                name="email_empresa"
+                class="form-control"
+                required>
+        </div>
+
+        <div class="form-group">
+            <label>Senha</label>
+
+            <input
+                type="password"
+                name="senha_empresa"
+                class="form-control"
+                required>
+        </div>
+
+        <button
+            type="submit"
+            name="entrar_empresa"
+            class="btn-primary">
+            Entrar
+        </button>
+
+    </form>
+
+</div>
+
+        <br>
+
+        <a href="#">
+            Esqueci minha senha
+        </a>
+
+    </div>
+
+</div>
+<script>
+
+function mostrarAluno()
+{
+    document.getElementById("formAluno").style.display = "block";
+    document.getElementById("formEmpresa").style.display = "none";
+
+    document.getElementById("btnAluno").classList.add("ativa");
+    document.getElementById("btnEmpresa").classList.remove("ativa");
+}
+
+function mostrarEmpresa()
+{
+    document.getElementById("formAluno").style.display = "none";
+    document.getElementById("formEmpresa").style.display = "block";
+
+    document.getElementById("btnEmpresa").classList.add("ativa");
+    document.getElementById("btnAluno").classList.remove("ativa");
+}
+
+</script>
+</body>
+</html>

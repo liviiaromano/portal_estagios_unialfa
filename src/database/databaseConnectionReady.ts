49a78e57b12f56cbb2e641e70290acd60c0c67a3
liviiaromano <liviaromanosi@gmail.com>
2026-06-14 20:52:20 -0300

    Delete src directory

--- src/database/databaseConnectionReady.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "./data-source";
-
-/**
- * Promise que resolve quando o TypeORM terminou `initialize()`.
- * Use `.then()` no `server` para montar rotas e `listen` só com o banco pronto.
- */
-export const databaseConnectionReady = AppDataSource.initialize();
